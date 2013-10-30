99c293a8adaf6268a8a457c5c46ed0da24a42b6b
Igor Perminov <code.pump@gmail.com>
2013-10-30 19:38:50 +0200

    Initial Commit

---
+++ todoTests/todoTests.h
@@ -0,0 +1,13 @@
+//
+//  todoTests.h
+//  todoTests
+//
+//  Created by Mac on 10/30/13.
+//  Copyright (c) 2013 Mac. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface todoTests : SenTestCase
+
+@end
---
+++ todoTests/todoTests.m
@@ -0,0 +1,32 @@
+//
+//  todoTests.m
+//  todoTests
+//
+//  Created by Mac on 10/30/13.
+//  Copyright (c) 2013 Mac. All rights reserved.
+//
+
+#import "todoTests.h"
+
+@implementation todoTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in todoTests");
+}
+
+@end
