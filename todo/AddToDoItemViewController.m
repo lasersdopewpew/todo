5bfd402bc3f77361378aea05132755540ee36b2b
Igor Perminov <code.pump@gmail.com>
2013-10-31 18:33:37 +0400

    Segoue unwind

---
+++ todo/AddToDoItemViewController.m
@@ -0,0 +1,40 @@
+//
+//  AddToDoItemViewController.m
+//  todo
+//
+//  Created by Mac on 10/31/13.
+//  Copyright (c) 2013 Mac. All rights reserved.
+//
+
+#import "AddToDoItemViewController.h"
+
+@interface AddToDoItemViewController ()
+
+@end
+
+@implementation AddToDoItemViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
---
+++ todo/TodoListViewController.h
@@ -0,0 +1,13 @@
+//
+//  TodoListViewController.h
+//  todo
+//
+//  Created by Mac on 10/31/13.
+//  Copyright (c) 2013 Mac. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TodoListViewController : UITableViewController
+
+@end
